4ad2121f2a4f75836b2744f7474ed69c87ab66f4
DanuKirilov <fazygoldmen@gmail.com>
2024-12-18 09:23:05 +0200

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static int Input(string mes)
+        {
+            Console.Write(mes);
+            return int.Parse(Console.ReadLine());
+        }
+        static void Main(string[] args)
+        {
+            int n = Input("n=");
+            Cicle(n);
+            Console.WriteLine(n);
+            Console.Read();
+        }
+        static void Cicle(int n)
+        {
+            int x = 1;
+            while (x<=n)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+        }
+    }
+}
