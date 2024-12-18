5df8cc06dc8f1c6ee60498620e01d20e421acf84
DanuKirilov <fazygoldmen@gmail.com>
2024-12-18 08:51:22 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Func()
+        {
+            int x = 1;
+            while (x <= 10) 
+            {
+                Console.WriteLine(x);
+                x = x + 1;//x+=1;x++
+            }
+        }
+        static void Main(string[] args)
+        {
+            Func();
+            Console.Read();
+        }
+    }
+}
